8be0332d915d4449588e3a2b9619406ca1c84e9e
Jonathon Stoddart <stoddart.jc@outlook.com>
2022-03-03 22:10:26 -0500

    create pet functionality added

--- client/src/pages/PetsPage.js
+++ client/src/pages/PetsPage.js
@@ -1,10 +1,45 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import '../App.css';
+import Axios from 'axios'
 import PetsTable from '../components/PetsTable';
 
 function PetsPage() {
 
     const [showTable, openTable] = useState(false)
+
+    const [name, setName] = useState('')
+    const [species, setSpecies] = useState('')
+    const [breed, setBreed] = useState('')
+    const [birthYear, setBirthYear] = useState('')
+    const [birthMonth, setBirthMonth] = useState('')
+    const [birthDay, setBirthDay] = useState('')
+    const [weight, setWeight] = useState('')
+    const [sex, setSex] = useState('')
+    const [clientID, setClientID] = useState('')
+
+
+    const createPet = () => {
+        console.log('hello!!!');
+        Axios.post('http://localhost:3001/api/insert', {
+            name: name,
+            species: species,
+            breed: breed,
+            birthYear: birthYear,
+            birthMonth: birthMonth,
+            birthDay: birthDay,
+            weight: weight,
+            sex: sex,
+            clientID: clientID
+        }).then(()=> {
+            alert('successful insert');
+        });
+    };
+
+    const clear = () => {
+        alert('hello');
+        console.log('hello');
+    }
+
     return (
         <div className="App">
             <h1>Search or Create Pets</h1>
@@ -15,28 +50,30 @@ function PetsPage() {
                 <tbody>
                     <tr>
                         <td>Name:</td>
-                        <td><input type="text" name="petName" /></td>
+                        <td><input type="text" name="name" onChange={((e)=> {
+                            setName(e.target.value)
+                        })} /></td>
                         <td>*Species:</td>
-                        <td><input type="text" name="petSpecies" /></td>
+                        <td><input type="text" name="species" /></td>
                     </tr>
                     <tr>
                         <td>Breed:</td>
-                        <td><input type="text" name="petBreed" /></td>
+                        <td><input type="text" name="breed" /></td>
                         <td>*Birth Year:</td>
-                        <td><input type="number" name="petBirthYear" /></td>
+                        <td><input type="number" name="birthYear" /></td>
                     </tr>
                     <tr>
                         <td>Birth month:</td>
-                        <td><input type="number" name="petBirthMonth" /></td>
+                        <td><input type="number" name="birthMonth" /></td>
                         <td>Birth day:</td>
-                        <td><input type="number" name="petBirthDay" /></td>
+                        <td><input type="number" name="birthDay" /></td>
                     </tr>
                     <tr>
                         <td>Weight:</td>
-                        <td><input type="number" name="petWeight" /></td>
+                        <td><input type="number" name="weight" /></td>
                         <td>*Sex:</td>                     
                         <td>
-                            <select id="petSex" name="petSex">
+                            <select id="sex" name="sex">
                                 <option hidden disabled selected value></option>
                                 <option value="male">Male</option>
                                 <option value="female">Female</option>
@@ -45,12 +82,13 @@ function PetsPage() {
                     </tr>
                     <tr>
                         <td>*Owner ID:</td>
-                        <td><select id="petOwner" name="petOwner" /></td>
+                        <td><select id="clientID" name="clientID" /></td>
                     </tr>
                 </tbody>
                 <tfoot>*Required</tfoot>
             </table>
-            <button onClick={() => alert("This is the Create Pets Button!")}>Create</button>
+            <button onClick={createPet}>Create</button>
+            <button onClick={clear}>Clear</button>
             
             <table id="SearchPets">
                 <thead>
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require('cors');
 const app = express();
-const mysql = require('mysql')
+const mysql = require('mysql');
 
 const db = mysql.createPool({
     connectionLimit : 10,
@@ -8,13 +10,46 @@ const db = mysql.createPool({
     user            : 'cs340_stoddjon',
     password        : '9198',
     database        : 'cs340_stoddjon'
-})
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get('/', (req, res)=> {
+app.get("/",(req, res) => {
+    res.send("hello world");
+    console.log("done")
+});
 
+app.get("/api/get", (req, res) => {
+    const sqlSelect = "SELECT * FROM Pets";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    });
+});
+
+app.post('/api/insert', (req, res)=> {
+    
+    console.log(req.body.name);
+
+    const name = req.body.name;
+    const species = req.body.species;
+    const breed = req.body.breed;
+    const birthYear = req.body.birthYear;
+    const birthMonth = req.body.birthMonth;
+    const birthDay = req.body.birthDay;
+    const weight = req.body.weight;
+    const sex = req.body.sex;
+    const clientID = req.body.clientID;
+
+    const sqlInsert = "INSERT INTO Pets (name, species, breed, birthYear, birthMonth, birthDay, weight, sex, clientID) VALUES (?,?,?,?,?,?,?,?,?)";
+
+    db.query(sqlInsert, [name, species, breed, birthYear, birthMonth, birthDay, weight, sex, clientID], (err, result)=> {
+        console.log(err);
+    });
 });
 
 app.listen(3001, () => {
-    console.log('running on port 3001')
+    console.log("running on port 3001");
 });
